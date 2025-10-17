f40522cdc8f288b82449d21b5676a5b1a544a74f
Jumpei Tanikawa <jumpei@JumpeinoMacBook-Air.local>
2025-10-17 22:53:34 +0900

    feat: Implement log manager.

--- server/server.go
+++ server/server.go
@@ -2,16 +2,20 @@ package server
 
 import (
 	"simple_db/file"
+	"simple_db/log"
 )
 
 type SimpleDB struct {
 	fileManager *file.Manager
+	logManager  *log.Manager
 }
 
 func NewSimpleDB(dirName string, blockSize int32, buffSize int32) *SimpleDB {
 	fileManager, _ := file.NewManager(dirName, blockSize)
+	logManager, _ := log.NewManager(fileManager, "simpledb.log")
 
 	return &SimpleDB{
 		fileManager: fileManager,
+		logManager:      logManager,
 	}
 }
