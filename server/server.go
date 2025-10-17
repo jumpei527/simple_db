7cfa2154ef836c77016c2c3c06ec4575128b5686
Jumpei Tanikawa <jumpei@JumpeinoMacBook-Air.local>
2025-10-17 22:57:02 +0900

    feat: Implement buffer manager.

--- server/server.go
+++ server/server.go
@@ -3,19 +3,24 @@ package server
 import (
 	"simple_db/file"
 	"simple_db/log"
+	"simple_db/buffer"
 )
 
 type SimpleDB struct {
 	fileManager *file.Manager
 	logManager  *log.Manager
+	bufferManager *buffer.Manager
 }
 
 func NewSimpleDB(dirName string, blockSize int32, buffSize int32) *SimpleDB {
 	fileManager, _ := file.NewManager(dirName, blockSize)
-	logManager, _ := log.NewManager(fileManager, "simpledb.log")
+	logManager, _ := log.NewManager(fileManager, "simple_db.log")
+	bufferManager := buffer.NewManager(fileManager, logManager, buffSize)
+
 
 	return &SimpleDB{
-		fileManager: fileManager,
-		logManager:      logManager,
+		fileManager:  fileManager,
+		logManager:   logManager,
+		bufferManager: bufferManager,
 	}
 }
